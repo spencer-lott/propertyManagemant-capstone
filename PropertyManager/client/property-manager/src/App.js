80f83ba0ab8230b0ab4be6ffb6c4370d794b4203
Spencer Lott <spklott@gmail.com>
2023-07-28 11:46:30 -0400

    made react app

---
+++ PropertyManager/client/property-manager/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import { BrowserRouter } from "react-router-dom"
+// import logo from './logo.svg';
+
+function App() {
+  return (
+    <BrowserRouter>
+    What's up buddy?
+    </BrowserRouter>
+  );
+}
+
+export default App;
